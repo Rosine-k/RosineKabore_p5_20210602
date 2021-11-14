5bcd867c041819959b99cbd0334fbc35306e9b5e
Rosine <kabore.rosine75@gmail.com>
2021-11-14 10:58:33 +0100

    modif fetch

--- js/script.js
+++ js/script.js
@@ -4,9 +4,6 @@ const createProduct = (data) =>{
     //divisez le prix par 100 
     data.price = data.price / 100;
 
-    //formater le prix
-    let price = priceFormat(data.price);
-
     return `<div class="col-sm-6">
               <a href="produit.html?id=${data._id}">
                     <div class="card">
@@ -30,6 +27,6 @@ fetch("http://localhost:3000/api/cameras")
         
         let camera = createProduct(value);
 
-           document.getElementById("card-camera").innerHTML += camera;   
+           document.querySelector(".card-camera").innerHTML += camera;   
         }   
     }); 
