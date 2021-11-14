846f8e4c54f9d73de3fe7a4411531ee2a7e6701f
Rosine <kabore.rosine75@gmail.com>
2021-11-14 01:04:06 +0100

    modif fetch

--- js/script.js
+++ js/script.js
@@ -1,22 +1,34 @@
-//récupération des produits à partir de l'API et affichage
-fetch('http://localhost:3000/api/cameras')
+const createProduct = (data) =>{
+
+    //divisez le prix par 100 
+    data.price = data.price / 100;
+
+    //formater le prix
+    let price = priceFormat(data.price);
+
+    return `<div class="col-sm-6">
+              <a href="produit.html?id=${data._id}">
+                    <div class="card">
+                        <img class="card-img-top" src="${data.imageUrl}" width="250" height="250" alt="camera">
+                        <div class="card-body bgc-primary">
+                            <h3 class="card-title black">${data.name}</h3>
+                            <h4 class="card-price black">${data.price} €</h4>
+                            <p class="card-text">Plus de détails</p>
+                        </div>
+                    </div>       
+                </a> 
+            </div>` ;
+}
+
+//récupération des produits à partir de l'API
+fetch("http://localhost:3000/api/cameras")
    .then(response => response.json())
    .then(response => {
         
        for (let value of response) {
         
         let product = createProduct(value);
-           document.querySelector(".card-camera") .innerHTML +=    `<div class="col-sm-6">
-                                                                        <a href="produit.html?id=${camera._id}">
-                                                                            <div class="card">
-                                                                                <img class="card-img-top" src="${camera.imageUrl}" width="250" height="250" alt="zurss">
-                                                                                <div class="card-body bgc-primary">
-                                                                                   <h3 class="card-title black">${camera.name}</h3>
-                                                                                   <h4 class="card-price black">${camera.price / 100} €</h4>
-                                                                                   <p class="card-text">Plus de détails</p>
-                                                                               </div>
-                                                                            </div>       
-                                                                        </a> 
-                                                                    </div>` ;
+
+           document.getElementById("card-camera").innerHTML += product;   
         }   
     }); 
