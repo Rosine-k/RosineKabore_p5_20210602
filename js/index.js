3ab7cfd4370a16110e02ea923bea6852bd123c06
Rosine <kabore.rosine75@gmail.com>
2022-06-08 20:33:49 +0200

    modifs fonctions

--- js/index.js
+++ js/index.js
@@ -1,30 +1,71 @@
-// création du produit
-const createCamera = (data) =>{
+const { response } = require("express");
 
-    return `<figure class="col-sm-4">
-               <a href="produit.html?id=${data._id}">
-                    <img class="img-appareil" src="${data.imageUrl}" width="150" height="150" alt="camera">
-                    <figurecaption class="vignette">
-                        
-                        <div class="info">
-                            <h3 class="title black">${data.name}</h3>
-                            <h4 class="price black">${formatPrice(data.price)} €</h4>
-                            <button class="btn btn-dark text">Voir le produit</button>
-                        </div>
-                    </figurecaption>       
-                </a> 
-            </figure>` ;
+// création du produit
+const showCamera = (data) =>{
+
+    if(data==null || data=="") {
+        messageForUser('Attention les donneés à afficher sont incorrectes','index.js -> createCamera');
+    }
+
+    let figure      = document.createElement('figure');
+    let lien        = document.createElement('a'); 
+    let img         = document.createElement('img'); 
+    let figureCap   = document.createElement('figurecaption');
+    let div         = document.createElement('div');
+    let h3          = document.createElement('h3');
+    let h4          = document.createElement('h4');
+    let btn         = document.createElement('button');
+
+    document.querySelector('figure').className       = "col-sm-4";
+    document.querySelector('lien').attributes        = "produit.html?id=${data._id}";
+    document.querySelector('img').className          = "img-appareil";
+    document.querySelector('img').attributes         = "${data.imageUrl}";
+    document.querySelector('img').width              = "150";
+    document.querySelector('img').height             = "150";
+    document.querySelector('img').alt                = "camera";
+    document.querySelector('figureCap').className    = "vignette";
+    document.querySelector('lien').className         = "info";
+    document.querySelector('h3').className           = "title", "black";
+    document.querySelector('h3').value               = "${data.name}";
+    document.querySelector('h4').value               = "${formatPrice(data.price)}";
+    document.querySelector('h4').className           = "price", "black";
+    document.querySelector('btn').className          = "btn", "btn-dark", "text";
+    document.querySelector('btn').textContent        = "Voir le produit";
+
+    document.body.append(figure);
+    document.body.appendChild(lien);
+    document.body.appendChild(img);
+    document.body.appendChild(figureCap);
+    document.body.appendChild(div);
+    document.body.appendChild(h3);
+    document.body.appendChild(h4);
+    document.body.appendChild(btn);
+    
+}
+
+function showDatas(datas) {
+    for (let datas of response) {
+        
+        let camera = showCamera(value);
+        //return datas;
+
+        document.querySelector(".card-camera").innerHTML += camera;   
+    }   
 }
 
 //récupération des produits à partir de l'API
-fetch(URL_API)
-   .then(response => response.json())
-   .then(response => {
-        
-       for (let value of response) {
-        
-            let camera = createCamera(value);
+function getData() {
+    fetch(URL_API)
+    .then(response => response.json())
+    .then(response => {
+            return response;
+        }); 
+}
 
-            document.querySelector(".card-camera").innerHTML += camera;   
-        }   
-    }); 
+function main() {
+    let datas = getData();   
+    showDatas(datas);
+}
+main();
+
+//showCamera('');
